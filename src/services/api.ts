118d3812921230163e6b6f578d2b2739a6043d1b
João Gabriel <jgcustodio2005@gmail.com>
2025-04-02 17:46:18 -0300

    feat(auth): half of the auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,43 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'https://gym-backend-production-bce0.up.railway.app'
+    process.env.NEXT_PUBLIC_API_URL || 'https://gym-backend-production-bce0.up.railway.app'
+
+export async function loginUser(email: string, password: string) {
+    const res = await fetch(`${API_URL}/user/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'applications/json' },
+        body: JSON.stringify({ email, password })
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) throw new Error(data.error || data.message || 'Erro ao fazer login')
+
+    return data
+}
+
+export async function registerUser(name: string, email: string, password: string, height_cm?: number, weight_kg?: number) {
+    const res = await fetch(`${API_URL}/user/register`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'applications/json' },
+        body: JSON.stringify({ name, email, password, height_cm, weight_kg })
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) throw new Error(data.error || data.message || 'Erro ao registrar')
+
+    return data
+}
+
+export async function getLogedUser(token: string) {
+    const res = await fetch(`${API_URL}/user/register`, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` }
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) throw new Error(data.error || data.message || 'Erro ao buscar o usuário logado')
+
+    return data
+}
