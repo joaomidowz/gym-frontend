530886b339ef28ba483e8657f613f71e9fcbe65e
João Gabriel <jgcustodio2005@gmail.com>
2025-04-03 15:26:17 -0300

    feat(user): implement full user service with streak and CRUD

---
+++ src/services/user.ts
@@ -0,0 +1,98 @@
+import { API_URL } from "./api";
+
+export async function getUserById(id: number, token: string) {
+  const res = await fetch(`${API_URL}/user/${id}`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao pegar o usuário");
+
+  return data;
+}
+
+export async function getAllUsers(token: string) {
+  const res = await fetch(`${API_URL}/user`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao procurar usuário");
+
+  return data;
+}
+
+export async function updateUser(
+  id: number,
+  token: string,
+  updates: {
+    name?: string;
+    email?: string;
+    password?: string;
+    height_cm?: number;
+    weight_kg?: number;
+  }
+) {
+  const res = await fetch(`${API_URL}/user/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(updates),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao editar conta");
+
+  return data;
+}
+
+export async function deleteUser(id: number, token: string) {
+  const res = await fetch(`${API_URL}/user/${id}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao deletar conta");
+
+  return data;
+}
+
+export async function getStreak(token: string) {
+  const res = await fetch(`${API_URL}/user/streak`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao ver streak");
+
+  return data;
+}
+
+export async function saveStreak(token: string) {
+  const res = await fetch(`${API_URL}/user/streak/save`, {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.error || data.message || "Erro ao salvar streak");
+
+  return data;
+}
